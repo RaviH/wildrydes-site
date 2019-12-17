92c659022ab2460a5aa01f37574436df23ae5d44
Ravi Hasija <ravi.hasija@charter.com>
2019-12-17 12:16:19 -0800

    Added user pool id, client id, region

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_02ASxLhEF', // e.g. us-east-2_uXboG5pAb
+        userPoolId: ' us-west-2_7vR3fUu3K', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '300rih9v8e6or0fbf60h08cja5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
